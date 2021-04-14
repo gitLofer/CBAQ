1dd143f1a6643ea14da2ff5f019b7a5c65335dc3
gitLofer <pilipovic.petar@jjzmaj.edu.rs>
2021-04-14 12:59:41 +0200

    Jezici moguci na bot-u
    Changelog:
    - Dodat users.data koji cuva user ID od svih
    - user_storage.py dodat sa svim funkcijama potrebne za uprvavljanje sa users.data
    - bot.py podesen da koristi alias-e
    
    TODO: Povezati ostale biblioteke na bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,75 @@
+import requests
+import json
+
+import user_storage as us
+
 import os
 import discord # pip install discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+
+def get_json_from_api(cityName, APItoken):
+    r = "https://api.waqi.info/feed/" + cityName + "/?token=" + APItoken
+    return (requests.get(r)).json()
+
+def get_city_info(cityName, lang):
+    cityJSON = get_json_from_api(cityName, waqiToken)
+    cityName = cityName.title()
+    if (cityJSON['status'] == 'ok'):
+        if lang == 'RS':
+            return "Kvalitet vazduha u gradu " + cityName + " je " + str(cityJSON['data']['aqi'])
+        elif lang == 'EN':
+            return "Air quality in " + cityName + " is " + str(cityJSON['data']['aqi'])
+    else:
+        if lang == 'RS':
+            return "Uneli ste nevazeci grad!"
+        elif lang == 'EN':
+            return "The requested city could not be found."
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+waqiToken = "e95e9dd0a620f4d84415424d036fc493e4059e45"
 
 intents = discord.Intents.default()
 intents.members = True
 
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='?', intents=intents)
+prefix = '?'
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+users = us.load_users()
 
 @bot.event
 async def on_ready():
+    print(users)
     print('Connected to Discord!')
 
+@bot.command(name='aqi', alias='time', help='Prikazi AQI za neki grad / Show the AQI for a given city')
+async def aqi(ctx, arg1):
+    print(users)
+    user_id = ctx.message.author.id
+    lang = us.user_lang(user_id, users)
+    print(lang)
+    answ = get_city_info(arg1, lang)
+    await ctx.send(answ)
+
+@bot.command(name='lang', help='For English: EN, ENGISH, ENGLESKI\nZa srpski: RS, SERBIAN, SRPSKI')
+async def lang(ctx, arg1):
+    arg1 = arg1.upper()
+    user_id = ctx.author.id
+    if arg1 == 'RS' or arg1 == 'SRPSKI' or arg1 == 'SERBIAN':
+        us.remove_user_id(user_id, users)
+        await ctx.send("Jezik namesten na srpski.")
+        return
+    elif arg1 == 'EN' or arg1 == 'ENGLESKI' or arg1 == 'ENGLISH':
+        us.add_user_id(user_id, users)
+        await ctx.send("Language changed to English.")
+        return
+    await ctx.send("Uneli ste nevazecu opciju. Uradite \"" + prefix + "help lang\" da saznate vise.\nYou've entered an invalid option. Do \"" + prefix + "help lang\" to see all valid options")
+
+# Alijasi za komande
+bot.command(name="weather", pass_context=True)(aqi.callback)
+
 bot.run(TOKEN)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ user_storage.py
@@ -0,0 +1,36 @@
+import bisect
+import pickle
+
+def add_user_id(user_id, user_list):
+    if user_list == []:
+        user_list.append(user_id)
+    else:
+        user_list = bisect.bisect_left(user_list, user_id)
+    save_users(user_list)
+    return
+
+def remove_user_id(user_id, user_list):
+    try:
+        del user_list[bisect.bisect_left(user_list, user_id)]
+        save_users(user_list)
+        return
+    except EOFError:
+        return
+
+def load_users():
+    with open('users.data', 'rb') as filehandle:
+        try:
+            user_list = pickle.load(filehandle)
+        except EOFError:
+            user_list = []
+        return user_list
+
+def save_users(user_list):
+    with open('users.data', 'wb') as filehandle:
+        pickle.dump(user_list, filehandle)
+        return
+
+def user_lang(user_id, user_list):
+    if user_id in user_list:
+        return 'EN'
+    return 'RS'
